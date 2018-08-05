7e7ee36c37cef7bb227eccb019e3cebf5b1129cd
Geethanjali <geethanjali.prabhu@gmail.com>
2018-08-05 13:15:15 -0400

    Completed Sprite Automation

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config ={
 	mode:{
@@ -15,7 +16,11 @@ var config ={
 	}
 }
 
-gulp.task('createSprite',function(){
+gulp.task('beginClean',function(){
+	return del(['./app/assets/sprite','./app/assets/images/sprite']);
+});
+
+gulp.task('createSprite',['beginClean'],function(){
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 	.pipe(svgSprite(config))
 	.pipe(gulp.dest('./app/assets/sprite/'));
@@ -32,4 +37,8 @@ gulp.task('copySpriteGraphic',['createSprite'],function(){
 	.pipe(gulp.dest('./app/assets/images/sprite'));
 });
 
-gulp.task('icons',['createSprite','copySpriteCSS','copySpriteGraphic']);
+gulp.task('endClean',['copySpriteCSS','copySpriteGraphic'],function(){
+	return del(['./app/assets/sprite']);
+});
+
+gulp.task('icons',['beginClean','createSprite','copySpriteCSS','copySpriteGraphic','endClean']);
