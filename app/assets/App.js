7f9dae104ffeda3d2fc5ce41682d370bdbc7c272
Geethanjali <geethanjali.prabhu@gmail.com>
2018-08-12 18:18:00 -0400

    Completed RevealOnScroll

--- app/assets/App.js
+++ app/assets/App.js
@@ -1,4 +1,8 @@
 import MobileMenu from './scripts/modules/MobileMenu';
+import RevealOnScroll from './scripts/modules/RevealOnScroll';
+import $ from 'jquery';
 
-var mobileMenu=new MobileMenu();
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");
 
