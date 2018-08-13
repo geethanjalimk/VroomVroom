88b8f0e87f7f54f5efc30dfde79122c53aca193f
Geethanjali <geethanjali.prabhu@gmail.com>
2018-08-13 15:36:37 -0400

    Completed Sticky Header

--- app/assets/App.js
+++ app/assets/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './scripts/modules/MobileMenu';
 import RevealOnScroll from './scripts/modules/RevealOnScroll';
+import StickyHeader from './scripts/modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
+var stickyHeader = new StickyHeader();
 
