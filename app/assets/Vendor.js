6843eacaaafc9d42ab230a2d7684b70c6730190b
Geethanjali <geethanjali.prabhu@gmail.com>
2018-08-22 10:40:26 -0400

    Completed browser suppourt

--- app/assets/Vendor.js
+++ app/assets/Vendor.js
@@ -1,1 +1,3 @@
+import "./scripts/modernizr";
+import "picturefill";
 import "lazysizes";
--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
 rename = require('gulp-rename'),
-del = require('del');
+del = require('del'),
+svg2png =require('gulp-svg2png');
 
 var config ={
 	mode:{
 		css:{
+			variables: {
+				replaceSvgWithPng: function(){
+					return function(sprite,render){
+						return render(sprite).split('.svg').join('.png');
+					}
+				}
+			},
 			sprite: 'sprite.svg',
 			render:{
 				css: {
@@ -26,14 +34,20 @@ gulp.task('createSprite',['beginClean'],function(){
 	.pipe(gulp.dest('./app/assets/sprite/'));
 });
 
+gulp.task('createSvg2Png',['createSprite'],function(){
+	return gulp.src('./app/assets/sprite/css/*.svg')
+	.pipe(svg2png())
+	.pipe(gulp.dest('./app/assets/sprite/css'));
+});
+
 gulp.task('copySpriteCSS',['createSprite'],function(){
 	return gulp.src('./app/assets/sprite/css/*.css')
 	.pipe(rename('_sprite.css'))
 	.pipe(gulp.dest('./app/assets/modules'));
 });
 
-gulp.task('copySpriteGraphic',['createSprite'],function(){
-	return gulp.src('./app/assets/sprite/css/**/*.svg')
+gulp.task('copySpriteGraphic',['createSvg2Png'],function(){
+	return gulp.src('./app/assets/sprite/css/**/*.{svg,png}')
 	.pipe(gulp.dest('./app/assets/images/sprite'));
 });
 
@@ -41,4 +55,4 @@ gulp.task('endClean',['copySpriteCSS','copySpriteGraphic'],function(){
 	return del(['./app/assets/sprite']);
 });
 
-gulp.task('icons',['beginClean','createSprite','copySpriteCSS','copySpriteGraphic','endClean']);
+gulp.task('icons',['beginClean','createSprite','createSvg2Png','copySpriteCSS','copySpriteGraphic','endClean']);
